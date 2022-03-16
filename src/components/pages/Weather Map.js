89d00dca7ebb8be2849913e54d07a452280d61c0
Justin Conger <jhconger@GMAIL.COM>
2022-03-16 18:33:55 -0500

    still trying to fix back button to return to react app

--- src/components/pages/Weather Map.js
+++ src/components/pages/Weather Map.js
@@ -1,17 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../App.css';
 
 
-export default function WeatherMap() {
-     useEffect(() => {
-         window.location.href = ('https://jhconger.github.io/weather_map/')
-     }, []);
 
-    return false;
- }
+    export default function WeatherMap() {
+            useEffect(() => {
+                // window.location.href = ('https://jhconger.github.io/weather_map/')
 
-// export const Test = () => (
-//     <div className="">
-//         <button onClick={BrowserRouter.goBack}>Back</button>
-//     </div>
-// )
+                    window.location.href = ('https://jhconger.github.io/weather_map/');
+
+            }, []);
+
+           return null
+        }
